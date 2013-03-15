3935c360613c751ce51fe6b6eb053dd40b8a7dea
Marat Bedoev <marat@marat-bedoev.net>
2013-03-15 20:57:37 +0100

    Update oxviewconfig_isal.php

--- copy_this/modules/ag-isal/extend/oxviewconfig_isal.php
+++ copy_this/modules/ag-isal/extend/oxviewconfig_isal.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2013 Stefan & Alexander Kludt, Marat Bedoev
+ * Copyright (c) 2013 Aggrosoft GbR & Marat Bedoev
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
