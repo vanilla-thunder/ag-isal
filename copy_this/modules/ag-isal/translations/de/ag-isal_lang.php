435728a23b8eaf25f313f3a1e770f2171b7f4b22
Joscha Krug <krug@marmalade.de>
2013-05-26 23:54:22 +0200

    Added multilang handling and moved block to new location

---
+++ copy_this/modules/ag-isal/translations/de/ag-isal_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$sLangName  = "Deutsch";
+
+$aLang = array(
+    'charset'                   => 'UTF-8',
+    'AG_ISAL_NO_MORE_PRODUCTS'  => 'Keine weiteren Produkte in dieser Ansicht.',
+    'AG_ISAL_LOADING'           => 'Lade weitere Produkte ...',
+    'AG_ISAL_TOP'               => 'Nach oben',
+);
